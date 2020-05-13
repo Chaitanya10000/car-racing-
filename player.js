15f8e88cf7359b33458ded222665bf9e5628443d
Chaitanya10000 <62645920+Chaitanya10000@users.noreply.github.com>
2020-05-13 19:02:00 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,23 @@
+class Player{
+constructor(){
+  }
+
+updatecount(count){
+    database.ref('/').update({
+        playercount:count
+    });
+
+}
+
+  update(name){
+      var playerindex = "player" + playercount;
+      database.ref(playerindex)
+      .set({name:name});
+  }
+
+  getcount(){
+      var playercountref = database.ref('playercount');
+      playercountref.on("value",function(data){playercount = data.val();})
+  }
+
+}
